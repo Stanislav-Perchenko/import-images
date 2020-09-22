e28ad703983ba40654066b07a0b912dcbfef5a6c
Stanislav-Perchenko <stanislav.perchenko@gmail.com>
2020-09-22 23:06:35 +0300

    Receive and display selected images

--- app/src/main/java/com/alperez/demo/importimages/MainActivity.kt
+++ app/src/main/java/com/alperez/demo/importimages/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.alperez.demo.importimages
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import com.alperez.demo.importimages.widget.ImageImportActionView
+import com.alperez.importimages.ImportImageActivity
 import com.alperez.importimages.LoadImageRequester
 import com.alperez.importimages.model.ImageAspectRatio
 import com.alperez.importimages.model.ImageImportModel
+import com.squareup.picasso.Picasso
 
 /**
  * Created by stanislav.perchenko on 22.09.2020 at 19:49.
@@ -65,6 +70,32 @@ class MainActivity : BaseActivity() {
 
         loadImageRequester = LoadImageRequester(this, resources.getString(R.string.dialog_title_import_document_image))
     }
+
+
+    /***********************************************************************************************
+     * Receiving selected images
+     */
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if ((resultCode != Activity.RESULT_CANCELED) && (data?.hasExtra(ImportImageActivity.RESULT_COVERIMAGE) == true)) {
+            val model = data.getParcelableExtra<ImageImportModel>(ImportImageActivity.RESULT_COVERIMAGE)
+            when (requestCode) {
+                REQUEST_LOAD_IMAGE_ID_CARD_FRONT -> {
+                    mImageIdCardFront = model
+                    vIdCardFrontSideSelector.setSelectedImage(model)
+                    vActionSubmitIdCardImages.isEnabled = (mImageIdCardBack != null)
+                }
+                REQUEST_LOAD_IMAGE_ID_CARD_BACK -> {
+                    mImageIdCardBack = model
+                    vIdCardBackSideSelector.setSelectedImage(model)
+                    vActionSubmitIdCardImages.isEnabled = (mImageIdCardFront != null)
+                }
+                else -> throw RuntimeException("WTF! This should not happen by design!")
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+
 
 
     private fun submitSelectedDocumentImages() {
